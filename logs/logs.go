f2893785f789af830e0ee10976569c5acb16af43
elandcloud <elandcloud>
2016-12-07 03:15:57 +0800

    modify log.go

--- logs/logs.go
+++ logs/logs.go
@@ -1,10 +1,6 @@
 package logs
 
-import (
-	"fmt"
-	"runtime"
-	"strings"
-)
+import "fmt"
 
 const (
 	Black = uint8(iota + 30)
@@ -36,28 +32,7 @@ func (c CustomerColor) Println(a ...interface{}) {
 	s := fmt.Sprint(a...)
 	c.doPrint(s)
 }
-
 func (c CustomerColor) doPrint(a interface{}) {
-	// isDebug := os.Getenv("WXSHOPDEBUG")
-	// if len(isDebug) == 0 && c.ColorNo != Red {
-	// 	return
-	// }
 
-	funcName, file, line, ok := runtime.Caller(2)
-
-	if ok {
-		base := "/best/wxshop"
-		i := strings.Index(file, base)
-		shoutFile := file[i+len(base):]
-
-		fileInfo := fmt.Sprintf("-->%v:%d %s", shoutFile, line, runtime.FuncForPC(funcName).Name())
-		colorFileInfo := fmt.Sprintf("\033[%dm%s\033[0m", Magenta, fileInfo)
-		fmt.Printf("\033[%dm%v\033[0m", c.ColorNo, a)
-		fmt.Println(colorFileInfo)
-
-	} else {
-		colorFileInfo := fmt.Sprintf("\033[%dm%v\033[0m", Red, "-->(Unknown debug info)")
-		fmt.Printf("\033[%dm%v\033[0m", c.ColorNo, a)
-		fmt.Println(colorFileInfo)
-	}
+	fmt.Printf("\033[%dm%v\033[0m", c.ColorNo, a)
 }
--- main.go
+++ main.go
@@ -1,22 +1,24 @@
 package main
 
 import (
-	"net/http"
 	"gorouter/logs"
+	"net/http"
 	//"gorouter/api"
 	"github.com/labstack/echo"
 )
+
 var (
-e = echo.New()
+	e = echo.New()
 )
+
 func main() {
-	
+
 	e.GET("/", func(c echo.Context) error {
+		logs.Warning.Println("hello good")
 		return c.String(http.StatusOK, "Hello echo api")
 	})
 	RegisterUserApi()
 	logs.Warning.Println("RegisterUserApi end")
-	
+
 	e.Start(":1111")
 }
-
